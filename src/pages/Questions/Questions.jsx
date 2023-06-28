0e74f9384aad9ee579facfe38d61b58d9d7fe1bf
ongcangco <pongcangco57@gmail.com>
2023-06-28 11:24:23 -0500

    Render questions

--- src/pages/Questions/Questions.jsx
+++ src/pages/Questions/Questions.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function Questions({data:{question, correct_answer, incorrect_answer} }) {
+export default function Questions({data:{question, correct_answer, incorrect_answers} }) {
     return (
        <>
-        <div className=''>
+        <div className='question'>
           <h1>{question}</h1>
         </div>
         <div className='main-button'>
           <button className='choice-button'>{correct_answer}</button>
-          <button className='choice-button'>{incorrect_answer}</button>
-          <button className='choice-button'>{incorrect_answer}</button>
-          <button className='choice-button'>{incorrect_answer}</button>
+          <button className='choice-button'>{incorrect_answers[0]}</button>
+          <button className='choice-button'>{incorrect_answers[1]}</button>
+          <button className='choice-button'>{incorrect_answers[2]}</button>
           
           
         </div>
